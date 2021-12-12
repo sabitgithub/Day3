5ea0e8200d3bcca624d9c77ddb6d90be3d2677c9
sabitgithub <waliurrahmansabit@gmail.com>
2021-12-13 00:13:03 +0600

    initial commit

---
+++ lession3_ifelse.py
@@ -0,0 +1,20 @@
+"""
+IF statement
+________
+
+if condition: 
+    true statement
+else :
+    false statement
+
+"""
+
+x=int(input('Please first value  : '))
+y=int(input('Please second value : '))
+
+if x>y:
+    print("x is big")
+elif x==y:
+    print("x & y both are equal")
+else:
+    print("Y is big")        
---
+++ lession3_operator.py
@@ -0,0 +1,37 @@
+"""
+Operator
+_________
+1. Arthematic Operator: 
+        Addition +
+        Substraction -
+        Multiplication *
+        Division /
+        Remainder %
+        Integer Division //
+        Exponent **
+
+ 2. Relational Operator
+        Greater Than >
+        Greater Than Or Equal >=
+        Less Than <
+        Less Than Or Equal <=
+        Equal To ==
+        Not Equal To !=
+
+3. Logical Operator
+        and
+        or
+        not      
+
+"""
+
+x=float(input('Enter Value for x :'))
+y=float(input('Enter Value for y :'))
+
+print(f"The Addition is {x+y}")
+print(f"The Subtraction is {x-y}")
+print(f"The Multiplication is {x*y}")
+print(f"The Division is {x/y}")
+print(f"Integer Division is {x//y}")
+print(f"The Remainder is {x%y}")
+print(f"The Exponent is {x**y}")
